1df9ef2f368bdcbf23d81f5e589a4e29fedc3ed1
Squire <esq.berrouz@gmail.com>
2021-05-26 10:06:24 +0500

    начал логику открытия меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,12 @@
 const tabs = document.querySelectorAll('.nation__tab');
 const tabsContent = document.querySelectorAll('.nation__discription');
+// const menuButton = document.querySelector('.nav__toggle');
+// const menu = document.querySelector('.nav__list');
+
+// menuButton.addEventListener('click', (evt) => {
+//     evt.preventDefault();
+//     menu.classList.toggle('nav__list--open');
+// })
 
 const getContent = (arr, i) => {
 	arr.forEach(item => {
